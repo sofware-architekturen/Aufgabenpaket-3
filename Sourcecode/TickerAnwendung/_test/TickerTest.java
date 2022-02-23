a58da010d64b5fa8244c62bd916146858e4c6fc7
VChouliak <chouliak@hotmail.com>
2022-02-23 11:59:23 +0100

    Implement MVC

--- Sourcecode/TickerAnwendung/_test/TickerTest.java
+++ Sourcecode/TickerAnwendung/_test/TickerTest.java
@@ -1,46 +1,29 @@
 package _test;
 
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import controller.Console;
-import model.Ticker;
-import view.JTickerDisplay;
+import controller.TickerController;
+import model.ObservableTicker;
+import view.ViewObserver;
 
-// Test- und Steuerprogramm für den Ticker
-// Oldstyle-Steuerung über Konsolenbefehle
+// Test- und Steuerprogramm fï¿½r den Ticker
+// Oldstyle-Steuerung ï¿½ber Konsolenbefehle
 public class TickerTest
 {
 
   public static void main(String[] args) throws Exception
   {
-    JTickerDisplay display = new JTickerDisplay();
-    display.registerActionListenerOnStartButton( new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e)
-      {
-       System.out.println("Start-Button pushes");
-      }
-    });
+    ViewObserver display = new ViewObserver();
+    ObservableTicker ticker = new ObservableTicker();
+    TickerController tickerController = new TickerController(ticker);
     
-    display.registerActionListenerOnStopButton( new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e)
-      {
-       System.out.println("Stop-Button pushes");
-      }
-    });
+    ticker.register(display);
     
-    display.registerActionListenerOnResetButton( new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e)
-      {
-       System.out.println("Reset-Button pushes");
-      }
-    });
+    display.registerActionListenerOnStartButton(tickerController);
+    display.registerActionListenerOnStopButton(tickerController);  
+    display.registerActionListenerOnResetButton(tickerController);
     
-    Ticker ticker = new Ticker();
+    
     
     while( true )
     {
@@ -68,10 +51,10 @@ public class TickerTest
       }
       else if( input.equalsIgnoreCase("help")  )
       {
-        System.out.println("Folgende Kommandos werder unterstützt: ");
+        System.out.println("Folgende Kommandos werder unterstï¿½tzt: ");
         System.out.println(" start - Startet den Ticker ");
         System.out.println(" stop - Stoppt den Ticker ");
-        System.out.println(" reset - Setzt den Ticker zurück ");
+        System.out.println(" reset - Setzt den Ticker zurï¿½ck ");
         System.out.println(" show - Zeigt den aktuellen Status des Ticker im Display");
         System.out.println(" exit - beendet die Anwendung ");
       }
